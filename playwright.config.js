df2e7c577de5e0407bed418c851d516ee4e218c8
Aleksei Perchukov <aleksei.perchukov@solbeg.com>
2024-11-20 15:36:57 +0100

    + workflow_dispatch in pipeline

--- playwright.config.js
+++ playwright.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   testDir: 'tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   workers: 1,
   reporter: [["line"], ["allure-playwright",
     {
