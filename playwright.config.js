b1abef7f51549535983e9d4d4c424c6a2d7f1e65
Aleksei Perchukov <aleksei.perchukov@solbeg.com>
2024-11-21 01:29:33 +0100

    Workers 12

--- playwright.config.js
+++ playwright.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
-  workers: 6,
+  workers: 12,
   reporter: [["line"], ["allure-playwright",
     {
       detail: false,
